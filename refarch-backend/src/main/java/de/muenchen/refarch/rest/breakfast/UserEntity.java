cf439677d4bd9cb6ad18f5843f670b2b112d67d4
bodmer02 <184080611+bodmer02@users.noreply.github.com>
2025-09-19 09:53:15 +0200

    verknüpfung entitäten

--- refarch-backend/src/main/java/de/muenchen/refarch/rest/breakfast/UserEntity.java
+++ refarch-backend/src/main/java/de/muenchen/refarch/rest/breakfast/UserEntity.java
@@ -1,10 +1,15 @@
 package de.muenchen.refarch.rest.breakfast;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import de.muenchen.refarch.common.BaseEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+
+import java.util.Set;
+
 
 @Entity
 // Definition of getter, setter, ...
@@ -13,21 +18,18 @@ import lombok.*;
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
-public class BreakfastEntity extends BaseEntity {
+public class UserEntity extends BaseEntity {
 
-    @ManyToOne
-    @JoinColumn(name = "name", referencedColumnName = "name")
-    private UserEntity user;
+    @JsonIgnore
+    @OneToMany(mappedBy = "user")
+    private Set<BreakfastEntity> breakfastEntities;
 
     @Column(nullable = false, length = 50)
     @NotNull @Size(min = 1, max = 50) private String name;
 
     @Column(nullable = false, length = 50)
-    @NotNull @Size(min = 1, max = 50) private String product;
-
-    @NotNull private double price;
+    @NotNull @Size(min = 1, max = 50) private String id;
 
 }
-
 
 
---
+++ refarch-frontend/src/types/UserBreakfast.ts
@@ -0,0 +1,4 @@
+export default interface UserBreakfast {
+    id: string;
+    name: string;
+}
